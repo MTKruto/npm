9f6abb2b1ff3f7ebad029062d71a7f15c6f2409d
Roj <ez@roj.im>
2023-05-11 14:52:19 +0300

    Initial commit

---
+++ transform.ts
@@ -0,0 +1,41 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.35.0/mod.ts";
+
+const version = Deno.args[0];
+
+if (!version) {
+  console.error("Version not provided.");
+  Deno.exit(1);
+}
+
+await emptyDir("./dist");
+
+await build({
+  entryPoints: ["./MTKruto/mod.ts"],
+  outDir: "./dist",
+  typeCheck: false,
+  shims: {
+    custom: [{
+      module: "./deno_global.ts",
+      globalNames: ["Deno"],
+    }],
+  },
+  compilerOptions: {
+    lib: ["esnext", "dom", "dom.iterable"],
+  },
+  packageManager: "pnpm",
+  package: {
+    name: "mtkruto",
+    version,
+    description: "An attempt to write a Deno-native MTProto client",
+    author: "Roj <rojserbest@icloud.com>",
+    license: "LGPL-3.0-or-later",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/MTKruto/MTKruto.git",
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync("LICENSE", "dist/LICENSE");
+    Deno.copyFileSync("README.md", "dist/README.md");
+  },
+});
