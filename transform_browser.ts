4afba20035a6b382aa3f7c8f42ecc6e7ec37dbd9
Roj <rojvv@icloud.com>
2026-03-16 18:20:21 +0300

    Re-add browser transform script

---
+++ transform_browser.ts
@@ -0,0 +1,51 @@
+import { build, emptyDir } from "jsr:@deno/dnt@0.42.3";
+
+const version = Deno.args[0];
+if (!version) {
+  console.error("Version not provided.");
+  Deno.exit(1);
+}
+
+await emptyDir("./dist");
+
+await build({
+  entryPoints: [
+    "mod.ts",
+    {
+      name: "./1_utilities",
+      path: "1_utilities.ts",
+    },
+  ],
+  outDir: "./dist",
+  typeCheck: false,
+  test: false,
+  shims: {},
+  compilerOptions: {
+    lib: ["ESNext", "DOM", "ESNext.AsyncIterable"],
+  },
+  packageManager: "pnpm",
+  package: {
+    name: "@mtkruto/browser",
+    version,
+    description: "MTKruto for browsers",
+    author: "Roj <rojvv@icloud.com>",
+    license: "LGPL-3.0-or-later",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/MTKruto/MTKruto.git",
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync("COPYING", "dist/COPYING");
+    Deno.copyFileSync("COPYING.LESSER", "dist/COPYING.LESSER");
+    Deno.copyFileSync("README.md", "dist/README.md");
+  },
+  filterDiagnostic(diagnostic) {
+    if (
+      diagnostic.file?.fileName.endsWith("fmt/colors.ts")
+    ) {
+      return false;
+    }
+    return true;
+  },
+});
